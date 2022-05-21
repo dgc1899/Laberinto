9fed5d521bc001c466408d2240153825d3aea0fe
JesusArellano00 <84939846+JesusArellano00@users.noreply.github.com>
2022-05-20 17:47:52 -0700

    Nivel 1
    Este es el nivel 1

---
+++ Assets/Scripts/Acelerometro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acelerometro : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float velocidad = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        rb.AddForce(tilt * velocidad);
+    }
+}
