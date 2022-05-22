071e5910884fad4edbc40608859860bfa4af3799
JesusArellano00 <84939846+JesusArellano00@users.noreply.github.com>
2022-05-22 02:44:11 -0700

    Controles
    Ya sirve el control y cuando pierdes o ganas:)

---
+++ Assets/Scripts/Trampa.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampa : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Juego.setIsDeadTrue();
+        Debug.Log("Perdiste, pa");
+    }
+}
